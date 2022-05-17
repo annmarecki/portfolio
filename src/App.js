6e3591f406db005e260ff338d7d7125a2bf50490
Ania Marecki <aniamarecki@gmail.com>
2022-05-17 19:43:29 -0400

    scss added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <div className="card">
+        <div className="welcome">Welcome to</div>
+        <div className="year">
+          <span>Ann Marecki's Portfolio Is In Progress!</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

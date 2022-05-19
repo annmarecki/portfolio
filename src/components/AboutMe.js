9d6e9e8ef9cae6d828bd822aabd8bbf29d1e75c8
Ania Marecki <aniamarecki@gmail.com>
2022-05-19 19:22:29 -0400

    about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-const aboutme = () => <></>;
-
 export default function AboutMe() {
   return (
-    <div>
-      <div className="content">{aboutme()}</div>
+    <div className="homepage">
+      <img className="headshot" src={require("../images/ania.png")}></img>
+      <img
+        className="headshot"
+        src={require("../images/rooseveltIsland.jpeg")}
+      ></img>
+      <h1>Who Am I?</h1>
+      <h3>
+        Hello World! My name is Ann Marecki but you can call me Ania. I am a
+        full stack developer based out of New York City and I'm bringing the
+        creativity of my artistic pastimes and the customer focused precision
+        from my previous career to the tech industry!
+      </h3>
+      <h1>What do I stand for?</h1>
+      <h1>What skills do I have?</h1>
+      <h2>Front-End</h2>
+      <h3>Javasdcipt • React • React Native • Redux • HTML5 • CSS</h3>
+      <h2>Back-End</h2>
+      <h3>Node.js • Express • PostgreSQL • Firebase • Sequelize</h3>
+      <h2>Other</h2>
+      <h3>Curiousity • Ingenuity • Work Ethic • Teamwork • Leadership</h3>
     </div>
   );
 }

84cd3c9d13b31e2214e3579f77a86fbe872b2016
Ania Marecki <aniamarecki@gmail.com>
2022-05-23 18:38:03 -0400

    contrast issues

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,16 +11,16 @@ export default function AboutMe() {
             Hello World! My name is Ann but you can call me
           </p>
           <h1 className="name">Ania</h1>
-          <h3>
+          <p>
             I am a full stack developer based out of New York City and I'm
             bringing the creativity of my artistic pastimes and the customer
             focused precision from my previous career to the tech industry.
-          </h3>
-          <h2>Front-End</h2>
+          </p>
+          <h1>Front-End</h1>
           <p>Javascript • React • React Native • Redux • HTML5 • CSS</p>
-          <h2>Back-End</h2>
+          <h1>Back-End</h1>
           <p>Node.js • Express • PostgreSQL • Firebase • Sequelize</p>
-          <h2>Other</h2>
+          <h1>Other</h1>
           <p>Curiousity • Ingenuity • Work Ethic • Teamwork • Leadership</p>
         </div>
       </div>

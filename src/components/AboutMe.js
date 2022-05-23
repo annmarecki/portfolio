d70567e678117024b9d32b8f5a7e6516b368eeb7
Ania Marecki <aniamarecki@gmail.com>
2022-05-23 17:54:41 -0400

    aboutmedone

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,27 +4,32 @@ import "../App.css";
 
 export default function AboutMe() {
   return (
-    <div className="homepage">
-      <img className="headshot" src={require("../images/ania.png")}></img>
-      <img
-        className="headshot"
-        src={require("../images/rooseveltIsland.jpeg")}
-      ></img>
-      <h1>Who Am I?</h1>
-      <h3>
-        Hello World! My name is Ann Marecki but you can call me Ania. I am a
-        full stack developer based out of New York City and I'm bringing the
-        creativity of my artistic pastimes and the customer focused precision
-        from my previous career to the tech industry!
-      </h3>
-      <h1>What do I stand for?</h1>
-      <h1>What skills do I have?</h1>
-      <h2>Front-End</h2>
-      <h3>Javasdcipt • React • React Native • Redux • HTML5 • CSS</h3>
-      <h2>Back-End</h2>
-      <h3>Node.js • Express • PostgreSQL • Firebase • Sequelize</h3>
-      <h2>Other</h2>
-      <h3>Curiousity • Ingenuity • Work Ethic • Teamwork • Leadership</h3>
+    <div className="aboutme">
+      <div className="aboutmecontent">
+        <div className="contentborder">
+          <p className="aboutmep">
+            Hello World! My name is Ann but you can call me
+          </p>
+          <h1 className="name">Ania</h1>
+          <h3>
+            I am a full stack developer based out of New York City and I'm
+            bringing the creativity of my artistic pastimes and the customer
+            focused precision from my previous career to the tech industry.
+          </h3>
+          <h2>Front-End</h2>
+          <p>Javascript • React • React Native • Redux • HTML5 • CSS</p>
+          <h2>Back-End</h2>
+          <p>Node.js • Express • PostgreSQL • Firebase • Sequelize</p>
+          <h2>Other</h2>
+          <p>Curiousity • Ingenuity • Work Ethic • Teamwork • Leadership</p>
+        </div>
+      </div>
+      <div>
+        <img
+          className="aboutmephoto"
+          src={require("../images/rooseveltIsland.jpeg")}
+        ></img>{" "}
+      </div>
     </div>
   );
 }

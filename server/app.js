a15cfbeed8f2482300c6d8c96c596bee606a0a16
Ania Marecki <aniamarecki@gmail.com>
2022-06-01 12:53:46 -0400

    react app buildpack

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ const publicPath = path.join(__dirname, "..", "public");
 const PORT = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "..", "public/index.html"))
-);
+app.get("/", (req, res) => res.send("hi"));
 
 app.listen(PORT, () => console.log(`Server port ${PORT} is up`));
